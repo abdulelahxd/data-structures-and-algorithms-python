4774efac25a88b4c8ba7de4527d373fd25bb8da3
abdulelahxd <abdulelahkassara@gmail.com>
2020-08-16 23:46:09 +0300

    solve challenge

--- data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
+++ data_structures_and_algorithms/challenges/array_reverse/array_reverse.py
@@ -1,12 +1,12 @@
+
 def reverse_array(arr):
     """Reverses a list
-
     Args:
         arr (list): python list
-
     Returns:
         [list]: list in reversed form
     """
     # put your function implementation here
-    return arr
-
+    # souluation
+    return arr[::-1]
+print(reverse_array(arr))
